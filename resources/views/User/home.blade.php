f853db49e7c1281de82b2a3c11a355ed864ff7ab
“Prasanna <prasanna.mane@bajajfinserv.in>
2023-07-02 16:05:11 +0530

    Update bug fixes

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,13 +21,16 @@
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
-    
+
+    <script src="https://kit.fontawesome.com/36366bcda2.js" crossorigin="anonymous"></script>
+
 </head>
+
 <body id="page-top" class="landing-page no-skin-config">
-<div class="navbar-wrapper">
+    <div class="navbar-wrapper">
         <nav class="navbar navbar-default navbar-fixed-top navbar-expand-md" role="navigation">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/login') }}">MATRIMONY</a>
+                <a class="navbar-brand" href="{{ url('/login') }}">MATRIMONY Login</a>
                 <div class="navbar-header page-scroll">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
                         <i class="fa fa-bars"></i>
@@ -36,65 +40,69 @@
                     <ul class="nav navbar-nav navbar-right">
                         <li><a class="nav-link page-scroll" href="#page-top">Home</a></li>
                         <li><a class="nav-link page-scroll" href="{{ url('/register') }}">Register</a></li>
-                        <li><a class="nav-link page-scroll" href="#team">Brides</a></li>
+                        <!--<li><a class="nav-link page-scroll" href="#team">Brides</a></li>
                         <li><a class="nav-link page-scroll" href="#testimonials">Grooms</a></li>
-                        <li><a class="nav-link page-scroll" href="#pricing">Pricing</a></li>
+                        <li><a class="nav-link page-scroll" href="#pricing">Pricing</a></li> -->
                         <li><a class="nav-link page-scroll" href="#contact">Contact</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
-</div>
-<div id="inSlider" class="carousel slide" data-ride="carousel" >
-    <ol class="carousel-indicators">
-        <li data-target="#inSlider" data-slide-to="0" class="active"></li>
-        <li data-target="#inSlider" data-slide-to="1"></li>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1>In Perfect Place,<br/>
-                    Discover Your Perfect<br/>
-                    Match,<br/>
-                        we are PP+ studio</h1>
-                    <p>Love is only essence of our relationship.</p>
-                    <p>
-                        <a class="btn btn-lg btn-primary" href="{{ url('/register') }}" role="button">Register</a>
-                        <a class="caption-link" href="perfectplace.in" role="button">Perfect Place Brand</a>
-                    </p>
-                </div>
-                <div class="carousel-image wow zoomIn">
-                    <img src="img/landing/laptop.png" alt="laptop"/>
-                </div>
-            </div>
-            <!-- Set background for slide in css -->
-            <div class="header-back one"></div>
-
-        </div>
-        <div class="carousel-item">
-            <div class="container">
-                <div class="carousel-caption blank">
-                    <h1>We create meaningful <br/> interfaces that inspire.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-                </div>
-            </div>
-            <!-- Set background for slide in css -->
-            <div class="header-back two"></div>
-        </div>
     </div>
-    <a class="carousel-control-prev" href="#inSlider" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#inSlider" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
+    <div id="inSlider" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#inSlider" data-slide-to="0" class="active"></li>
+            <li data-target="#inSlider" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Welcome to Eternal Bonds,<br />
+                            Find Your Perfect Match,<br />
+                            Where Love Lasts Forever</h1>
+                        <p>Love is the foundation of every beautiful relationship.</p>
+                        <p>
+                            <a class="btn btn-lg btn-primary" href="{{ url('/register') }}" role="button">Register</a>
+                            <a class="caption-link" href="https://perfectplace.in" role="button">Perfect Place</a>
+                        </p>
+                    </div>
+                    <div class="carousel-image wow zoomIn">
+                        <img src="img/landing/laptop.png" alt="laptop" />
+                    </div>
+                </div>
+                <!-- Set background for slide in css -->
+                <div class="header-back one"></div>
 
-<!--
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="carousel-caption blank">
+                        <h2>Discover Your Soul Mate</h2>
+                        <p>At Eternal Bonds, we believe in helping you find your soul mate, someone who completes you and shares your journey through life. Our platform is designed to connect individuals who are seeking a lifelong partnership based on love, understanding, and compatibility.</p>
+                        <p>With our advanced matching algorithms and comprehensive profiles, you can explore potential matches that align with your values, interests, and aspirations. We provide a safe and secure environment where you can connect and get to know each other at your own pace.</p>
+
+                        <p>
+                            <a class="btn btn-lg btn-primary" href="{{ url('/login') }}" role="button">Login</a>
+                            <a class="caption-link" href="https://perfectplace.in" role="button">Perfect Place</a>
+                        </p>
+                    </div>
+                </div>
+                <!-- Set background for slide in css -->
+                <div class="header-back two"></div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#inSlider" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#inSlider" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <!--
 <section id="features" class="container services">
     <div class="row">
         <div class="col-sm-3">
@@ -580,34 +588,34 @@
 
 </section>
 -->
-<section id="contact" class="gray-section contact">
-    <div class="container">
-        <div class="row m-b-lg">
-            <div class="col-lg-12 text-center">
-                <div class="navy-line"></div>
-                <h1>Contact Us</h1>
-                
+    <section id="contact" class="gray-section contact">
+        <div class="container">
+            <div class="row m-b-lg">
+                <div class="col-lg-12 text-center">
+                    <div class="navy-line"></div>
+                    <h1>Contact Us</h1>
+
+                </div>
             </div>
-        </div>
-        <div class="row m-b-lg justify-content-center">
-            <div class="col-lg-3 ">
-                <address>
-                    <strong><span class="navy">Perfect Place.</span></strong><br/>
-                    Viman Nagar Pune,<br/>
-                    411014<br/>
-                    <abbr title="Phone">P:</abbr> +91 9834896987
-                </address>
-            </div>
-            <div class="col-lg-4">
-                <!--<p class="text-color">
+            <div class="row m-b-lg justify-content-center">
+                <div class="col-lg-3 ">
+                    <address>
+                        <strong><span class="navy">Perfect Place.</span></strong><br />
+                        Viman Nagar Pune,<br />
+                        411014<br />
+                        <abbr title="Phone">P:</abbr> +91 9834896987
+                    </address>
+                </div>
+                <div class="col-lg-4">
+                    <!--<p class="text-color">
                     Consectetur adipisicing elit. Aut eaque, totam corporis laboriosam veritatis quis ad perspiciatis, totam corporis laboriosam veritatis, consectetur adipisicing elit quos non quis ad perspiciatis, totam corporis ea,
                 </p> -->
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <a href="mailto:info@perfectplace.in" class="btn btn-primary">Send us mail</a>
-                <!--
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <a href="mailto:info@perfectplace.in" class="btn btn-primary">Send us mail</a>
+                    <!--
                 <p class="m-t-sm">
                     Or follow us on social platform
                 </p>
@@ -619,85 +627,88 @@
                     <li class="list-inline-item"><a href="#"><i class="fa fa-linkedin"></i></a>
                     </li>
                 </ul> -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center m-t-lg m-b-lg">
+                    <p><strong>&copy; 2023-24 Perfect Place</strong>
+                        <!-- <br/> consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.-->
+                    </p>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-12 text-center m-t-lg m-b-lg">
-                <p><strong>&copy; 2023-24 Perfect Place</strong>
-                <!-- <br/> consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.-->
-            </p>
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
 
-<!-- Mainly scripts -->
-<script src="js/jquery-3.1.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
-<!-- Custom and plugin javascript -->
-<script src="js/inspinia.js"></script>
-<script src="js/plugins/pace/pace.min.js"></script>
-<script src="js/plugins/wow/wow.min.js"></script>
+    <!-- Custom and plugin javascript -->
+    <script src="js/inspinia.js"></script>
+    <script src="js/plugins/pace/pace.min.js"></script>
+    <script src="js/plugins/wow/wow.min.js"></script>
+
+    <script src="js/maxcdn.bootstrapcdn.com_bootstrap_4.5.2_js_bootstrap.min.js"></script>
 
 
-<script>
+    <script>
+        $(document).ready(function() {
 
-    $(document).ready(function () {
+            $('body').scrollspy({
+                target: '#navbar',
+                offset: 80
+            });
 
-        $('body').scrollspy({
-            target: '#navbar',
-            offset: 80
+            // Page scrolling feature
+            $('a.page-scroll').bind('click', function(event) {
+                var link = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $(link.attr('href')).offset().top - 50
+                }, 500);
+                event.preventDefault();
+                $("#navbar").collapse('hide');
+            });
         });
 
-        // Page scrolling feature
-        $('a.page-scroll').bind('click', function(event) {
-            var link = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(link.attr('href')).offset().top - 50
-            }, 500);
-            event.preventDefault();
-            $("#navbar").collapse('hide');
-        });
-    });
-
-    var cbpAnimatedHeader = (function() {
-        var docElem = document.documentElement,
-                header = document.querySelector( '.navbar-default' ),
+        var cbpAnimatedHeader = (function() {
+            var docElem = document.documentElement,
+                header = document.querySelector('.navbar-default'),
                 didScroll = false,
                 changeHeaderOn = 200;
-        function init() {
-            window.addEventListener( 'scroll', function( event ) {
-                if( !didScroll ) {
-                    didScroll = true;
-                    setTimeout( scrollPage, 250 );
+
+            function init() {
+                window.addEventListener('scroll', function(event) {
+                    if (!didScroll) {
+                        didScroll = true;
+                        setTimeout(scrollPage, 250);
+                    }
+                }, false);
+            }
+
+            function scrollPage() {
+                var sy = scrollY();
+                if (sy >= changeHeaderOn) {
+                    $(header).addClass('navbar-scroll')
+                } else {
+                    $(header).removeClass('navbar-scroll')
                 }
-            }, false );
-        }
-        function scrollPage() {
-            var sy = scrollY();
-            if ( sy >= changeHeaderOn ) {
-                $(header).addClass('navbar-scroll')
+                didScroll = false;
             }
-            else {
-                $(header).removeClass('navbar-scroll')
+
+            function scrollY() {
+                return window.pageYOffset || docElem.scrollTop;
             }
-            didScroll = false;
-        }
-        function scrollY() {
-            return window.pageYOffset || docElem.scrollTop;
-        }
-        init();
+            init();
 
-    })();
+        })();
 
-    // Activate WOW.js plugin for animation on scrol
-    new WOW().init();
-
-</script>
+        // Activate WOW.js plugin for animation on scrol
+        new WOW().init();
+    </script>
 
 </body>
+
 </html>
